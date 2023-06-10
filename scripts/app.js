e091df9e6cd57f10c265036f2bf5aac21deec698
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-11 00:43:04 +0530

    (first added variable and event listner to that variables in app.js)(Then created fuction for that event listners in config.js)(This fuction are for edit player info btn and cancel btn)

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+const PlayerConfigOverlayElement = document.getElementById('config-overlay');1
+const backDropElement = document.getElementById('backdrop');
+
+const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
+const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+
+
+editPlayer1BtnElement.addEventListener('click',openPlayerConfig);
+editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
+backDropElement.addEventListener('click', closePlayerConfig);
+
---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+function openPlayerConfig() {
+    PlayerConfigOverlayElement.style.display = 'block'
+    backDropElement.style.display='block'
+
+}
+
+
+
+function closePlayerConfig() {
+     PlayerConfigOverlayElement.style.display = 'none'
+     backDropElement.style.display='none'
+
+    
+}
