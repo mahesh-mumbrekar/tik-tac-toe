c574c1cc5ae4071ea1410c1c7069dff069cdfe40
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-13 17:00:14 +0530

    created 2 variables to store column and row of selected field and used that column and row no. to to upadated the previously crated array to to find winner

--- scripts/game.js
+++ scripts/game.js
@@ -23,5 +23,11 @@ function switchPlayer() {
 function selectGameField(event) {
     event.target.textContent = players[activePlayer].symbol;
     event.target.classList.add('disabled');
+
+    const selectedColumn = event.target.dataset.col-1;
+    const selectedRow = event.target.dataset.row-1;
+    gameData[selectedColumn][selectedRow] = activePlayer + 1;
+    console.log(gameData)
+
     switchPlayer();   
 }
