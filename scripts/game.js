224af03b9b385f47f1043a3efe1c4b7e1020576a
kwingston <mahesh.mummbrekar@gmail.com>
2023-06-13 16:13:24 +0530

    created new fucntion switchPlayer and called this fuction insider selectGameField fuction to switch player after every turn

--- scripts/game.js
+++ scripts/game.js
@@ -9,9 +9,17 @@ function startNewGame() {
     
 }
 
+function switchPlayer() {
+    if (activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+}
+
 
 function selectGameField(event) {
     event.target.textContent = players[activePlayer].symbol;
     event.target.classList.add('disabled');
-    
+    switchPlayer();   
 }
